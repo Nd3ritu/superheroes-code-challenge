1d778d1aa78c282d89ee6f92d45034c350c297d9
Matthew <+>
2024-01-23 11:19:59 +0300

    /heroes GET done

--- app/app.py
+++ app/app.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask,request,jsonify, make_response
 from flask_migrate import Migrate
+from flask_cors import CORS
 
-from models import db, Hero
+from models import db, Hero,Power,HeroPower
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
 @app.route('/')
 def home():
-    return ''
+    response_body = {
+        "Hey": "This is the superhero domain"
+    }
+
+    response = make_response(jsonify(response_body))
+    return response
+
+@app.route('/heroes')
+def heroes():
+    heroes_list = [hero.to_dict() for hero in Hero.query.all()]
+    return make_response(jsonify(heroes_list),200)    
 
 
 if __name__ == '__main__':
